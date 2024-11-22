fc41703db0d9641e7d71f2bd0cb901e745b4f121
machariamarigi <machariamarigi@gmail.com>
2024-11-22 16:47:18 +0300

    adds Notification model

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -4,3 +4,9 @@ type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
+
+type Notification struct {
+	From    User   `json:"from"`
+	To      User   `json:"to"`
+	Message string `json:"message"`
+}
